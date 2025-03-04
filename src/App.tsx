df5333f44d18b5a5652de6c21b7fcc50660e0c9c
George Zeng <63271460+georgezzeng@users.noreply.github.com>
2025-03-04 16:21:57 -0600

    Project Initialization

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { ThemeProvider } from '@/components/theme-provider.tsx'
+import { NavBar} from '@/components/NavBar.tsx'
+
+function App() {
+
+  return (
+    <>
+      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+        <NavBar/>
+      </ThemeProvider>
+
+    </>
+  )
+}
+
+export default App
